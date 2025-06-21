c095fd5c9cd43641f14a5303558ea11091443c1c
xxaqurm <tohanesterov003@gmail.com>
2025-06-22 00:21:15 +0700

    Решена 5 задача с помощью цепной дроби

--- C++/src/algorithms/solveDiophantine.cpp
+++ C++/src/algorithms/solveDiophantine.cpp
@@ -12,69 +12,78 @@
 
 using namespace std;
 
-tuple<mpz_class, mpz_class, mpz_class> extendedGcd(mpz_class a, mpz_class b) {
-    if (b == 0) {
-        return {a, 1, 0};
-    }
-
-    mpz_class gcd, x1, y1;
-    tie(gcd, x1, y1) = extendedGcd(b, a % b);
-    return {gcd, y1, x1 - (a / b) * y1}; 
-}
-
-void solveDiophantine(mpz_class a, mpz_class b, mpz_class c, mpz_class& x, mpz_class& y) {
-    /* Находит x, y для уравнения вида ax + by = d */
-    mpz_class gcd, x0, y0;
-    tie(gcd, x0, y0) = extendedGcd(abs(a), abs(b));
-    
-    if (c % gcd != 0) {
-        throw runtime_error("Нет решения.");
-        return;
-    }
-
-    mpz_class k = c / gcd;
-    x = x0 * k;
-    y = y0 * k;
-}
-
-vector<mpz_class> continuedFraction(mpz_class a, mpz_class b) {
-    /* Ищет цепную дробь для рационального числа a / b */
-    vector<mpz_class> cf;
+mpz_class my_gcd(mpz_class a, mpz_class b) {
+    /* Находит НОД(a, b) */
     while (b != 0) {
-        cf.push_back(a / b);
-        mpz_class quotient = a % b;
+        mpz_class remains = a % b;
         a = b;
-        b = quotient;
+        b = remains;
     }
-    return cf;
+    return a;
 }
 
-void printContinuedFraction(vector<mpz_class> cf) {
-    /* Выводит цепную дробь */
-    cout << "[";
-    for (size_t i = 0; i < cf.size(); i++) {
-        cout << cf[i];
-        if (i == 0 && cf.size() > 0) cout << "; ";
-        else if (i < cf.size() - 1) cout << ", ";
+void solveDiophantine(mpz_class a, mpz_class b, mpz_class c, mpz_class& x, mpz_class& y, vector<mpz_class>& chainFractions) {
+    /* Находит x, y для уравнения вида ax + by = d */
+    mpz_class gcd = my_gcd(my_gcd(a, b), c);
+    mpz_class finalCoeff = c / gcd;
+
+    a /= gcd;
+    b /= gcd;
+    c /= gcd;
+
+    mpz_class numerator = a;
+    mpz_class denominator = b;
+
+    int termCount = 0;
+    while (denominator != 1) {
+        chainFractions.push_back(numerator / denominator);
+
+        mpz_class remains = numerator % denominator;
+        
+        numerator = denominator;
+        denominator = remains;
+        termCount++;
     }
-    cout << "]" << endl;
+
+    mpz_class finalNumerator = 1;
+    mpz_class finalDenominator = chainFractions[termCount - 1];
+    for (int i = chainFractions.size() - 2; i >= 0; i--) {
+        finalNumerator = chainFractions[i] * finalDenominator + finalNumerator;
+        mpz_class temp = finalDenominator;
+        finalDenominator = finalNumerator;
+        finalNumerator = temp;
+    }
+
+    x = pow(-1, termCount - 1) * finalNumerator * finalCoeff;
+    y = pow(-1, termCount) * finalDenominator * finalCoeff;
 }
 
 void solve_dioph() {
     mpz_class a = 275, b = 145, d = 10;
     mpz_class x = 0, y = 0;
 
+    vector<mpz_class> chainFractions;
+
     string inputError = "Невозможно решить при заданных значениях.";
     try {
-        solveDiophantine(a, b, d, x, y);
+        solveDiophantine(a, b, d, x, y, chainFractions);
+        cout << "Найденный X: " << x << endl;
+        cout << "Найденный Y: " << y << endl;
+
+        cout << "Цепная дробь: [";
+        for (size_t i = 0; i < chainFractions.size(); i++) {
+            cout << chainFractions[i];
+            if (i == 0) {
+                cout << "; ";
+            } else if (i != chainFractions.size() - 1) {
+                cout << ", ";
+            }
+        }
+        cout << "]" << endl;
+        
         cout << "Решение уравнения: " << a << "*x + " << b << "*y = " << d << ":\n";
-        cout << "x = " << x << "\ny = " << y << endl;
+        cout << "x = " << x << "\ny = " << y << endl << endl;
     } catch(const exception& e) {
         userInputError(e, inputError);
     }
-
-    cout << "\nЦепная дробь для " << a << " / " << b << ":" << endl;
-    vector<mpz_class> contFraction = continuedFraction(a, b);
-    printContinuedFraction(contFraction);
-    cout << endl;
 }
